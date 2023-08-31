d30d804582e141b995f5481c0c5b39891e229b89
pc <nelsongomesdasilva1125@gmail.com>
2023-08-31 16:48:20 -0300

    commit

--- app/src/main/java/br/com/mt/Formcadastro.java
+++ app/src/main/java/br/com/mt/Formcadastro.java
@@ -5,22 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.tasks.OnCanceledListener;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Formcadastro extends AppCompatActivity {
 
     private EditText edit_nome,edit_email,edit_senha;
     private Button bt_cadastrar;
     String[] mensagens = {"Preencha todos os campos!", "Cadastro realizado com suceesso!"};
+    String usuarioID;
 
 
     @Override
@@ -44,13 +56,13 @@ public class Formcadastro extends AppCompatActivity {
                     snackbar.setTextColor(Color.BLACK);
                     snackbar.show();
                 }else{
-                    CadastrarUsuario();
+                    CadastrarUsuario(v);
                 }
             }
         });
     }
 
-    private void CadastrarUsuario(){
+    private void CadastrarUsuario(View v){
 
         String email = edit_email.getText().toString();
         String senha = edit_senha.getText().toString();
@@ -61,10 +73,62 @@ public class Formcadastro extends AppCompatActivity {
 
                 if (task.isSuccessful()) {
 
+                    SalvarDadosUsuario();
+
+                    Snackbar snackbar = Snackbar.make(v, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else{
+                     String erro;
+                     try {
+                         throw task.getException();
+
+                     }catch (FirebaseAuthWeakPasswordException e) {
+                         erro = "Digite uma senha com 6 ou mais caracteres";
+                     }catch (FirebaseAuthUserCollisionException e) {
+                         erro = "Conta já cadastrada";
+                     }catch (FirebaseAuthInvalidCredentialsException e) {
+                         erro = "E-mail inválido";
+                     }catch (Exception e){
+                         erro = "Erro ao cadastro usuário";
+                     }
+
+                    Snackbar snackbar = Snackbar.make(v, erro, Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
                 }
             }
         });
+    }
 
+    private void SalvarDadosUsuario() {
+        String nome = edit_nome.getText().toString();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> usuarios = new HashMap<>();
+        usuarios.put("nome", nome);
+
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.set(usuarios).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("db", "Sucesso ao salvar os dados");
+
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("db_error","Erro ao salvar os dados" + e.toString());
+
+                    }
+                });
     }
 
     private void IniciarComponentes(){
--- app/src/main/java/br/com/mt/Login.java
+++ app/src/main/java/br/com/mt/Login.java
@@ -1,15 +1,30 @@
 package br.com.mt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
     private Button text_tela_cadastro;
+    private EditText edit_email,edit_senha;
+    private Button bt_entrar;
+    private ProgressBar progressBar;
+    String[] mensagens = {"Preencha todos os campos!", "Login efetuado com suceesso!"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +40,42 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        bt_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (email.isEmpty() || senha.isEmpty()){
+                    Snackbar snackbar = Snackbar.make(v, mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                }else {
+                    AutenticarUsuario();
+                }
+            }
+        });
+    }
+    private void AutenticarUsuario(){
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+            }
+        });
     }
     private void IniciarComponentes(){
         text_tela_cadastro = findViewById(R.id.text_tela_cadastro);
+        edit_email = findViewById(R.id.edit_email);
+        edit_senha = findViewById(R.id.edit_senha);
+        bt_entrar = findViewById(R.id.bt_entrar);
+        progressBar = findViewById(R.id.progressbar);
     }
 }
