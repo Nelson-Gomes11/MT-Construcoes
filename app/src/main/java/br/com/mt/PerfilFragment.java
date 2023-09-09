c4e627c4998e9436e22a0ba3101a5f1eb7a04c84
pc <nelsongomesdasilva1125@gmail.com>
2023-09-09 12:58:07 -0300

    commit

--- app/src/main/java/br/com/mt/PerfilFragment.java
+++ app/src/main/java/br/com/mt/PerfilFragment.java
@@ -1,4 +1,4 @@
-package br.com.mt.ui.contato;
+package br.com.mt;
 
 import android.os.Bundle;
 
@@ -8,16 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import br.com.mt.R;
-
-
-public class ContatoFragment extends Fragment {
-
+public class PerfilFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contato, container, false);
+        return inflater.inflate(R.layout.fragment_perfil, container, false);
     }
 }
