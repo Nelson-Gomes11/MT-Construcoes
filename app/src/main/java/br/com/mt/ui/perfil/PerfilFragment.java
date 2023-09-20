f3b4975c7166bfc1ecc14086da121b54e470cb68
pc <nelsongomesdasilva1125@gmail.com>
2023-09-20 16:46:55 -0300

    commit

--- app/src/main/java/br/com/mt/ui/perfil/PerfilFragment.java
+++ app/src/main/java/br/com/mt/ui/perfil/PerfilFragment.java
@@ -1,23 +1,78 @@
 package br.com.mt.ui.perfil;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import br.com.mt.Login;
 import br.com.mt.R;
-
 
 public class PerfilFragment extends Fragment {
 
+    private TextView nomeUsuario, emailUsuario;
+    private Button btDeslogar;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String usuarioID;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_perfil, container, false);
+
+        nomeUsuario = view.findViewById(R.id.nomedoperfil);
+        emailUsuario = view.findViewById(R.id.perfil_email);
+        btDeslogar = view.findViewById(R.id.bt_deslogar);
+
+        if (getActivity() != null && getActivity().getActionBar() != null) {
+            getActivity().getActionBar().hide();
+        }
+
+        btDeslogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+                if (getActivity() != null) {
+                    getActivity().finish();
+                }
+            }
+        });
+
+        return view;
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onStart() {
+        super.onStart();
 
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        String email = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+        usuarioID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        DocumentReference documentReference = db.collection("Usuarios").document(usuarioID);
+        documentReference.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                if (documentSnapshot != null){
+                    nomeUsuario.setText(documentSnapshot.getString("nome"));
+                    emailUsuario.setText(email);
+                }
+            }
+        });
     }
 }
