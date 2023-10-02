b48574908801c83fa2883a3a9f1742e8cfb59d02
pc <nelsongomesdasilva1125@gmail.com>
2023-10-02 17:52:51 -0300

    commit

--- app/src/main/java/br/com/mt/ui/home/HomeFragment.java
+++ app/src/main/java/br/com/mt/ui/home/HomeFragment.java
@@ -1,9 +1,12 @@
 package br.com.mt.ui.home;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.ScrollView;
 import android.widget.Toast;
@@ -15,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -26,9 +30,11 @@ import br.com.mt.R;
 import br.com.mt.adapters.HomeAdapter;
 import br.com.mt.adapters.MaisAdapters;
 import br.com.mt.adapters.PromoAdapter;
+import br.com.mt.adapters.ShowAllAdapter;
 import br.com.mt.models.HomeCategory;
 import br.com.mt.models.MaisVend;
 import br.com.mt.models.PromoModel;
+import br.com.mt.models.ShowAllModel;
 
 public class HomeFragment extends Fragment {
 
@@ -41,11 +47,15 @@ public class HomeFragment extends Fragment {
     List<MaisVend> maisVendList;
     MaisAdapters maisAdapters;
 
+    //pesquisa
+    EditText search_box;
+    private List<ShowAllModel> showAllModelList;
+    private RecyclerView recyclerViewSearch;
+    private ShowAllAdapter showAllAdapter;
+
     //categoria
     List<HomeCategory> categoryList;
     HomeAdapter homeAdapter;
-
-
     List<PromoModel> promoModelList;
     PromoAdapter promoAdapter;
 
@@ -65,9 +75,9 @@ public class HomeFragment extends Fragment {
         scrollView.setVisibility(View.GONE);
 
 
-        maisVend.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        maisVend.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
         maisVendList = new ArrayList<>();
-        maisAdapters = new MaisAdapters(getActivity(),maisVendList);
+        maisAdapters = new MaisAdapters(getActivity(), maisVendList);
         maisVend.setAdapter(maisAdapters);
 
         db.collection("MaisVendidos")
@@ -85,14 +95,14 @@ public class HomeFragment extends Fragment {
                                 scrollView.setVisibility(View.VISIBLE);
                             }
                         } else {
-                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
-        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
         categoryList = new ArrayList<>();
-        homeAdapter = new HomeAdapter(getActivity(),categoryList);
+        homeAdapter = new HomeAdapter(getActivity(), categoryList);
         homeCatRec.setAdapter(homeAdapter);
 
         db.collection("HomeCategory")
@@ -108,15 +118,15 @@ public class HomeFragment extends Fragment {
                                 homeAdapter.notifyDataSetChanged();
                             }
                         } else {
-                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
 
-        promoRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        promoRec.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
         promoModelList = new ArrayList<>();
-        promoAdapter = new PromoAdapter(getActivity(),promoModelList);
+        promoAdapter = new PromoAdapter(getActivity(), promoModelList);
         promoRec.setAdapter(promoAdapter);
 
         db.collection("EmPromocao")
@@ -132,11 +142,69 @@ public class HomeFragment extends Fragment {
                                 promoAdapter.notifyDataSetChanged();
                             }
                         } else {
-                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getActivity(), "Error" + task.getException(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
 
+        recyclerViewSearch = root.findViewById(R.id.search_rec);
+        search_box = root.findViewById(R.id.search_box);
+        showAllModelList = new ArrayList<>();
+        showAllAdapter = new ShowAllAdapter(getContext(),showAllModelList);
+        recyclerViewSearch.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewSearch.setAdapter(showAllAdapter);
+        recyclerViewSearch.setHasFixedSize(true);
+        search_box.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                if (s.toString().isEmpty()) {
+                    showAllModelList.clear();
+                    showAllAdapter.notifyDataSetChanged();
+                } else {
+                    searchProduct(s.toString());
+                }
+
+            }
+        });
+
         return root;
+
     }
-}
+
+        private void searchProduct(String type) {
+
+            if (!type.isEmpty()) {
+
+                db.collection("AllProducts").whereEqualTo("type",type).get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                                if (task.isSuccessful() && task.getResult() != null) {
+                                    showAllModelList.clear();
+                                    showAllAdapter.notifyDataSetChanged();
+                                    for (DocumentSnapshot doc : task.getResult().getDocuments()){
+                                        ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                                        showAllModelList.add(showAllModel);
+                                        showAllAdapter.notifyDataSetChanged();
+                                    }
+                                }
+
+                            }
+                        });
+            }
+        }
+
+    }
+
