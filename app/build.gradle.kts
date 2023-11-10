5fe518d1f6ba4c92b3b1491b157fbc5990ea0c86
Nelson Gomes <nelsongomesdasilva1125@gmail.com>
2023-11-09 23:42:07 -0300

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation ("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
     implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation("com.google.firebase:firebase-messaging:23.2.1")
+    implementation ("com.github.dhaval2404:imagepicker:2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
